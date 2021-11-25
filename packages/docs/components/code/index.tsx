d2e222a7c54a72590511a35fb92bab289d754544
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-11-25 20:55:09 +0100

    feat: Add back button setup

--- packages/docs/components/code/index.tsx
+++ packages/docs/components/code/index.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useEffect } from 'react'
 import Prism from 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css'
 import 'prismjs/components/prism-jsx.min'
+import { setupPrism } from './setup'
 
 export type CodeProps = {
   code: string
@@ -11,6 +12,7 @@ export type CodeProps = {
 export const Code: FunctionComponent<CodeProps> = ({ code, language }) => {
   useEffect(() => {
     Prism.highlightAll()
+    setupPrism()
   }, [])
 
   return (
