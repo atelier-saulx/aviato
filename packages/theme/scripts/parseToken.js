41b4a5945f63592c79f684e38cd3164443705db3
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-11-26 23:18:40 +0100

    feat: Progress

--- packages/theme/scripts/parseToken.js
+++ packages/theme/scripts/parseToken.js
@@ -2,17 +2,23 @@
 import ora from 'ora'
 import chalk from 'chalk'
 
-const spinner = ora('Loading unicorns').start()
+const spinner = ora('Parsing Theme').start()
 
 const green = chalk.bold.green
 const log = (message) => console.log(green(message))
 
-log('Parsing')
+// Ask Maarten:
+// Why not this?
+// https://www.figma.com/community/plugin/888356646278934516/Design-Tokens
+// https://github.com/lukasoppermann/design-token-transformer
 
-// https://github.com/rihardsgravis/figma-parser
-
-log('Done parsing')
+// For now...
+// https://www.npmjs.com/package/token-transformer
 
 setTimeout(() => {
   spinner.stop()
-}, 500)
+
+  log('Done parsing')
+
+  process.exit()
+}, 1500)
