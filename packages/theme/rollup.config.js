36d636e7108725654875a2a0e4c49827d62335f6
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-11-26 12:55:52 +0100

    feat: Cleanup build

--- packages/theme/rollup.config.js
+++ packages/theme/rollup.config.js
@@ -1,21 +1,17 @@
 import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json'
+import packageJson from './package.json'
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: pkg.main,
+      file: packageJson.main,
       format: 'cjs',
     },
     {
-      file: pkg.module,
+      file: packageJson.module,
       format: 'es',
     },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
     typescript({
