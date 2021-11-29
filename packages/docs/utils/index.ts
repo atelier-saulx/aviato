4eea35c0af9fe5ea48cf4168f8faa3e2d352ff8e
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-11-29 13:08:48 +0100

    fix: Fix failing build on main branch

--- packages/docs/utils/index.ts
+++ packages/docs/utils/index.ts
@@ -1,26 +1,2 @@
-let inDevEnvironment = false
-
-if (process && process.env.NODE_ENV === 'development') {
-  inDevEnvironment = true
-}
-
-import {
-  LogLevel,
-  setupLogging as setupApplicationLogging,
-  log,
-  createConsoleAdapter,
-} from '@aviato/utils'
-
-const initialiseApplication = () => {
-  const logLevel = inDevEnvironment ? LogLevel.DEBUG : LogLevel.ERROR
-  const adapters = [createConsoleAdapter()]
-
-  setupApplicationLogging({
-    logLevel,
-    adapters,
-  })
-
-  log.debug('Application has been started.')
-}
-
-export { initialiseApplication, inDevEnvironment }
+export * from './log'
+export * from './misc'
