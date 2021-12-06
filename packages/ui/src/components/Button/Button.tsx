487ea184f59dab2ff99c2cfcbdf484191c345b4a
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-12-06 16:37:01 +0100

    feat: Closer to CSS approach

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React, { ElementRef, MouseEventHandler, useCallback } from 'react'
-import { css, DefaultProps, styled } from '~/theme'
+import { classNames, css, DefaultProps, styled } from '~/theme'
 import { noop } from '@aviato/utils'
 
 const ButtonStyles = css({
@@ -17,216 +17,172 @@ const ButtonStyles = css({
 
   variants: {
     type: {
-      primary: {},
-      ghost: {},
-      error: {},
-    },
-    mode: {
-      filled: {},
-      outlined: {},
-      transparent: {},
+      primary: {
+        '&.isFilled': {
+          color: '$PrimaryContrastHigh',
+          background: '$PrimaryMain',
+          border: '0px solid transparent',
+
+          '&:hover': {
+            background: '$PrimaryMainHover',
+          },
+          '&:active': {
+            background: '$PrimaryMainSelected',
+          },
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: '$ActionDisabledBackground',
+            border: '0px solid transparent',
+          },
+        },
+
+        '&.isOutlined': {
+          color: '$PrimaryMain',
+          border: '1px solid $PrimaryOutlineBorder',
+
+          '&:hover': {
+            backgroundColor: '$PrimaryLightHover',
+          },
+          '&:active': {
+            background: '$PrimaryLightSelected',
+          },
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: 'transparent',
+            border: '1px solid $ActionDisabledBackground',
+          },
+        },
+
+        '&.isTransparent': {
+          color: '$PrimaryMain',
+
+          '&:hover': {
+            backgroundColor: '$PrimaryLightHover',
+          },
+          '&:active': {
+            background: '$PrimaryLightSelected',
+          },
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: 'transparent',
+            border: '0px solid transparent',
+          },
+        },
+      },
+
+      ghost: {
+        '&.isFilled': {
+          color: '$TextPrimary',
+          background: '$ActionMain',
+          border: '0px solid transparent',
+
+          '&:hover': {
+            background: '$ActionMainHover',
+          },
+          '&:active': {
+            background: '$ActionMainSelected',
+          },
+
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: '$ActionDisabledBackground',
+            border: '0px solid transparent',
+          },
+        },
+
+        '&.isOutlined': {
+          color: '$TextPrimary',
+          border: '1px solid $OtherOutline',
+
+          '&:hover': {
+            backgroundColor: '$ActionMainHover',
+          },
+          '&:active': {
+            background: '$ActionMainSelected',
+          },
+
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: 'transparent',
+            border: '1px solid $ActionDisabledBackground',
+          },
+        },
+
+        '&.isTransparent': {
+          color: '$TextPrimary',
+
+          '&:hover': {
+            backgroundColor: '$ActionMainHover',
+          },
+          '&:active': {
+            background: '$ActionMainSelected',
+          },
+
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: 'transparent',
+            border: '0px solid transparent',
+          },
+        },
+      },
+
+      error: {
+        '&.isFilled': {
+          color: '$PrimaryContrastHigh',
+          background: '$ErrorMain',
+          border: '0px solid transparent',
+
+          '&:hover': {
+            background: '$ErrorMainHover',
+          },
+          '&:active': {
+            background: '$ErrorMainSelected',
+          },
+
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: '$ActionDisabledBackground',
+            border: '0px solid transparent',
+          },
+        },
+
+        '&.isOutlined': {
+          color: '$ErrorMain',
+          border: '1px solid $ErrorOutlineBorder',
+
+          '&:hover': {
+            backgroundColor: '$ErrorLightHover',
+          },
+          '&:active': {
+            background: '$ErrorLightSelected',
+          },
+
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: 'transparent',
+            border: '1px solid $ActionDisabledBackground',
+          },
+        },
+
+        '&.isTransparent': {
+          color: '$ErrorMain',
+
+          '&:hover': {
+            backgroundColor: '$ErrorLightHover',
+          },
+          '&:active': {
+            background: '$ErrorLightSelected',
+          },
+
+          '&:disabled': {
+            color: '$ActionDisabledContent',
+            background: 'transparent',
+            border: '0px solid transparent',
+          },
+        },
+      },
     },
   },
-
-  compoundVariants: [
-    // PRIMARY COMPOUND START
-    {
-      type: 'primary',
-      mode: 'filled',
-      css: {
-        color: '$PrimaryContrastHigh',
-        background: '$PrimaryMain',
-        border: '0px solid transparent',
-
-        '&:hover': {
-          background: '$PrimaryMainHover',
-        },
-        '&:active': {
-          background: '$PrimaryMainSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: '$ActionDisabledBackground',
-          border: '0px solid transparent',
-        },
-      },
-    },
-    {
-      type: 'primary',
-      mode: 'outlined',
-      css: {
-        color: '$PrimaryMain',
-        border: '1px solid $PrimaryOutlineBorder',
-
-        '&:hover': {
-          backgroundColor: '$PrimaryLightHover',
-        },
-        '&:active': {
-          background: '$PrimaryLightSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: 'transparent',
-          border: '1px solid $ActionDisabledBackground',
-        },
-      },
-    },
-    {
-      type: 'primary',
-      mode: 'transparent',
-      css: {
-        color: '$PrimaryMain',
-
-        '&:hover': {
-          backgroundColor: '$PrimaryLightHover',
-        },
-        '&:active': {
-          background: '$PrimaryLightSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: 'transparent',
-          border: '0px solid transparent',
-        },
-      },
-    },
-    // PRIMARY COMPOUND END
-
-    // GHOST COMPOUND START
-    {
-      type: 'ghost',
-      mode: 'filled',
-      css: {
-        color: '$TextPrimary',
-        background: '$ActionMain',
-        border: '0px solid transparent',
-
-        '&:hover': {
-          background: '$ActionMainHover',
-        },
-        '&:active': {
-          background: '$ActionMainSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: '$ActionDisabledBackground',
-          border: '0px solid transparent',
-        },
-      },
-    },
-    {
-      type: 'ghost',
-      mode: 'outlined',
-      css: {
-        color: '$TextPrimary',
-        border: '1px solid $OtherOutline',
-
-        '&:hover': {
-          backgroundColor: '$ActionMainHover',
-        },
-        '&:active': {
-          background: '$ActionMainSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: 'transparent',
-          border: '1px solid $ActionDisabledBackground',
-        },
-      },
-    },
-    {
-      type: 'ghost',
-      mode: 'transparent',
-      css: {
-        color: '$TextPrimary',
-
-        '&:hover': {
-          backgroundColor: '$ActionMainHover',
-        },
-        '&:active': {
-          background: '$ActionMainSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: 'transparent',
-          border: '0px solid transparent',
-        },
-      },
-    },
-    // GHOST COMPOUND END
-
-    // ERROR COMPOUND START
-    {
-      type: 'error',
-      mode: 'filled',
-      css: {
-        color: '$PrimaryContrastHigh',
-        background: '$ErrorMain',
-        border: '0px solid transparent',
-
-        '&:hover': {
-          background: '$ErrorMainHover',
-        },
-        '&:active': {
-          background: '$ErrorMainSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: '$ActionDisabledBackground',
-          border: '0px solid transparent',
-        },
-      },
-    },
-    {
-      type: 'error',
-      mode: 'outlined',
-      css: {
-        color: '$ErrorMain',
-        border: '1px solid $ErrorOutlineBorder',
-
-        '&:hover': {
-          backgroundColor: '$ErrorLightHover',
-        },
-        '&:active': {
-          background: '$ErrorLightSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: 'transparent',
-          border: '1px solid $ActionDisabledBackground',
-        },
-      },
-    },
-    {
-      type: 'error',
-      mode: 'transparent',
-      css: {
-        color: '$ErrorMain',
-
-        '&:hover': {
-          backgroundColor: '$ErrorLightHover',
-        },
-        '&:active': {
-          background: '$ErrorLightSelected',
-        },
-
-        '&:disabled': {
-          color: '$ActionDisabledContent',
-          background: 'transparent',
-          border: '0px solid transparent',
-        },
-      },
-    },
-    // ERROR COMPOUND END
-  ],
 })
 
 const StyledButton = styled('button', ButtonStyles)
@@ -258,13 +214,19 @@ export const Button = React.forwardRef<
     onClick()
   }, [])
 
+  const classes = classNames({
+    isFilled: mode === 'filled',
+    isOutlined: mode === 'outlined',
+    isTransparent: mode === 'transparent',
+  })
+
   return (
     <StyledButton
       ref={forwardedRef}
       type={type}
-      mode={mode}
       onClick={handleClick}
       disabled={disabled}
+      className={classes}
       {...remainingProps}
     >
       {children}
