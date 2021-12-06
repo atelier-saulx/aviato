e9fec2b9fbf2e53947dc7169feac767f04e2c577
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-12-06 22:37:58 +0100

    feat: Working text prototype

--- packages/ui/src/components/Text/Text.tsx
+++ packages/ui/src/components/Text/Text.tsx
@@ -1,26 +1,41 @@
-import React, { FunctionComponent } from 'react'
-import { styled } from '~/theme'
+import React from 'react'
+import { styled, CSS } from '~/theme'
 import { TextStyles } from './styles'
 
 import { FontWeight, FontColor, FontSize } from './types'
 
-const StyledText = styled('p', TextStyles)
+const TEXT_TAG = 'p'
 
-export type TextProps = {
+const StyledTitle = styled(TEXT_TAG, TextStyles)
+
+type BaseTextProps = {
   weight?: FontWeight
   color?: FontColor
   size?: FontSize
+  css?: CSS
 }
 
-export const Text: FunctionComponent<TextProps> = ({
-  children,
-  weight = 'Regular',
-  color = 'Primary',
-  size = 'Small',
-}) => {
+export type TextProps = React.ComponentProps<typeof TEXT_TAG> & BaseTextProps
+
+export const Text = React.forwardRef<
+  React.ElementRef<typeof TEXT_TAG>,
+  TextProps
+>((properties, forwardedRef) => {
+  const {
+    weight = 'Semibold',
+    color = 'Primary',
+    size = 'Medium',
+    ...remainingProps
+  } = properties
+
   return (
-    <StyledText weight={weight} color={color} size={size} alignment="start">
-      {children}
-    </StyledText>
+    <StyledTitle
+      as={TEXT_TAG}
+      weight={weight}
+      color={color}
+      size={size}
+      {...remainingProps}
+      ref={forwardedRef}
+    />
   )
-}
+})
