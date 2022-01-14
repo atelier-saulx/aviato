d3ad7ed82cd5a2b506b146f132228243b43b6f90
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-01-14 12:22:30 +0100

    feat: Add isomorpic Portal + cleanup

---
+++ packages/ui/src/components/Utilities/Portal.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode, ReactPortal, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { useIsomorphicEffect } from '@aviato/hooks'
+
+export interface PortalProps {
+  children: ReactNode
+  zIndex?: number
+  target?: HTMLElement | string
+  className?: string
+}
+
+export function Portal({
+  children,
+  zIndex = 1,
+  target,
+  className,
+}: PortalProps): ReactPortal {
+  const [mounted, setMounted] = useState(false)
+  const ref = useRef<HTMLElement>()
+
+  useIsomorphicEffect(() => {
+    setMounted(true)
+
+    ref.current = !target
+      ? document.createElement('div')
+      : typeof target === 'string'
+      ? document.querySelector(target)
+      : target
+
+    if (!target) {
+      document.body.appendChild(ref.current)
+    }
+
+    return () => {
+      !target && document.body.removeChild(ref.current)
+    }
+  }, [target])
+
+  if (!mounted) {
+    return null
+  }
+
+  return createPortal(
+    <div className={className} style={{ position: 'relative', zIndex }}>
+      {children}
+    </div>,
+    ref.current
+  )
+}
--- packages/ui/src/components/Utilities/index.ts
+++ packages/ui/src/components/Utilities/index.ts
@@ -1,1 +1,2 @@
 export * from './Conditional'
+export * from './Portal'
