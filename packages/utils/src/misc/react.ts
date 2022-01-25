9ab8e81422ad4c8db97cb0fd72b50e9c004f71b6
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-01-25 13:41:05 +0100

    refactor: Restructure code

--- packages/utils/src/misc/react.ts
+++ packages/utils/src/misc/react.ts
@@ -15,9 +15,9 @@ export function filterChildrenByType(
   children: ReactNode,
   type: JSXElementConstructor<any> | JSXElementConstructor<any>[]
 ) {
-  return (Children.toArray(children) as ReactElement[]).filter((item) =>
-    Array.isArray(type)
+  return (Children.toArray(children) as ReactElement[]).filter((item) => {
+    return Array.isArray(type)
       ? type.some((component) => component === item.type)
       : item.type === type
-  )
+  })
 }
