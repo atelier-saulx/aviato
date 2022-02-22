6dc81d41d75f103b8aa748a94e2e0a5bf750493c
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-02-22 12:16:28 +0100

    feat: Add more icons

---
+++ packages/ui/src/components/Icons/components/File.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SVGProperties } from '../types'
+
+const SvgFile = (props: SVGProperties) => {
+  return (
+    <svg width={16} height={16} fill="none" viewBox="0 0 16 16" {...props}>
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeOpacity={0.87}
+        strokeWidth={1.33333}
+        d="M8.66675 1.33325H4.00008C3.64646 1.33325 3.30732 1.47373 3.05727 1.72378C2.80722 1.97382 2.66675 2.31296 2.66675 2.66659V13.3333C2.66675 13.6869 2.80722 14.026 3.05727 14.2761C3.30732 14.5261 3.64646 14.6666 4.00008 14.6666H12.0001C12.3537 14.6666 12.6928 14.5261 12.9429 14.2761C13.1929 14.026 13.3334 13.6869 13.3334 13.3333V5.99992M8.66675 1.33325L13.3334 5.99992M8.66675 1.33325V5.99992H13.3334"
+      />
+    </svg>
+  )
+}
+
+export default SvgFile
--- packages/ui/src/components/Icons/components/Layers.tsx
+++ packages/ui/src/components/Icons/components/Layers.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SVGProperties } from '../types'
 
-const SvgDashboard = (props: SVGProperties) => {
+const SvgLayers = (props: SVGProperties) => {
   return (
     <svg width={16} height={16} fill="none" viewBox="0 0 16 16" {...props}>
       <path
@@ -9,27 +9,27 @@ const SvgDashboard = (props: SVGProperties) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeOpacity={0.87}
-        strokeWidth={1.33}
-        d="M2 2V14"
+        strokeWidth={1.33333}
+        d="M7.99992 1.33325L1.33325 4.66659L7.99992 7.99992L14.6666 4.66659L7.99992 1.33325Z"
       />
       <path
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeOpacity={0.87}
-        strokeWidth={1.33}
-        d="M14 14H2"
+        strokeWidth={1.33333}
+        d="M1.33325 11.3333L7.99992 14.6666L14.6666 11.3333"
       />
       <path
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeOpacity={0.87}
-        strokeWidth={1.33}
-        d="M4.66675 10.6667L8.16675 7.16667L10.5001 9.5L14.0001 6"
+        strokeWidth={1.33333}
+        d="M1.33325 8L7.99992 11.3333L14.6666 8"
       />
     </svg>
   )
 }
 
-export default SvgDashboard
+export default SvgLayers
