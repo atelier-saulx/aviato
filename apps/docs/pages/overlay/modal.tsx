1cf762c61de920b3497f9fbb4d82dcf39cd3f3c1
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-03-14 13:23:27 +0100

    chore: Add code examples

--- apps/docs/pages/overlay/modal.tsx
+++ apps/docs/pages/overlay/modal.tsx
@@ -45,7 +45,7 @@ const ModalPage = () => {
 
     return (
       <>
-        <Modal isOpen={isOpen} onClose={() => closeDialogAction()}>
+        <Modal isOpen={isOpen} onClose={closeDialogAction}>
           <Text>This is a simple modal.</Text>
         </Modal>
 
@@ -54,7 +54,7 @@ const ModalPage = () => {
     )
   }
 
-  const ShowModal = () => {
+  const ShowComplexModal = () => {
     const [isOpen, setIsOpen] = useState(false)
 
     const closeDialogAction = (wasConfirmed: boolean) => {
@@ -125,15 +125,126 @@ const ModalPage = () => {
 
       <NextText color="Secondary">Overlay with interactive elements.</NextText>
 
-      <ShowcaseComponent background="transparent">
+      <ShowcaseComponent
+        background="transparent"
+        codeBlock={`
+import { Modal, Button, Text } from '@aviato/ui'
+
+const [isOpen, setIsOpen] = useState(false)
+
+const closeDialogAction = () => {
+  setIsOpen(false)
+}
+
+return (
+  <>
+    <Modal isOpen={isOpen} onClose={closeDialogAction}>
+      <Text>This is a simple modal.</Text>
+    </Modal>
+
+    <Button onClick={() => setIsOpen(true)}>Open Simple Modal</Button>
+  </>
+)
+      `}
+      >
         <ShowSimpleModal />
       </ShowcaseComponent>
 
-      <ShowcaseComponent background="transparent">
-        <ShowModal />
+      <ShowcaseComponent
+        background="transparent"
+        codeBlock={`
+import { Modal, Button, Text, Group, Input } from '@aviato/ui'
+
+const [isOpen, setIsOpen] = useState(false)
+
+const closeDialogAction = (wasConfirmed: boolean) => {
+  setIsOpen(false)
+}
+
+const modalButtons: ModalButton[] = [
+  {
+    text: 'Cancel (Esc)',
+    type: 'outline',
+    hotkey: 'escape',
+    onClick: () => log.global.debug('Cancel'),
+  },
+  {
+    text: 'Register (Cmd+Enter)',
+    type: 'primary',
+    hotkey: 'cmd+enter',
+    onClick: () => log.global.debug('Register'),
+  },
+]
+
+const modalHotkeys: ModalHotkey[] = [
+  ['ctrl+t', () => log.global.debug('CTRL+T was pressed.')],
+]
+
+return (
+  <>
+    <Modal
+      isOpen={isOpen}
+      onClose={(wasConfirmed) => closeDialogAction(wasConfirmed)}
+      onConfirm={() => log.global.debug('Confirm and close')}
+      onCancel={() => log.global.debug('Close without confirming')}
+      buttons={modalButtons}
+      hotkeys={modalHotkeys}
+    >
+      <Text weight="semibold">Register</Text>
+
+      <Group direction="horizontal">
+        <Input
+          type="text"
+          placeholder="Type something here"
+          label="First Name"
+        />
+        <Input
+          type="text"
+          placeholder="Type something here"
+          label="Last Name"
+        />
+      </Group>
+    </Modal>
+
+    <Button onClick={() => setIsOpen(true)}>Open Modal</Button>
+  </>
+)
+      `}
+      >
+        <ShowComplexModal />
       </ShowcaseComponent>
 
-      <ShowcaseComponent>
+      <ShowcaseComponent
+        codeBlock={`
+const modalButtons: ModalButton[] = [
+  {
+    text: 'Register User (Cmd+O)',
+    type: 'primary',
+    hotkey: 'cmd+o',
+    onClick: () => log.global.debug('Register User'),
+  },
+]
+
+return (
+  <ModalElement buttons={modalButtons}>
+    <Text weight="semibold">Register</Text>
+
+    <Group direction="horizontal">
+      <Input
+        type="text"
+        placeholder="Type something here"
+        label="First Name"
+      />
+      <Input
+        type="text"
+        placeholder="Type something here"
+        label="Last Name"
+      />
+    </Group>
+  </ModalElement>
+)
+      `}
+      >
         <ShowModalElement />
       </ShowcaseComponent>
     </Page>
