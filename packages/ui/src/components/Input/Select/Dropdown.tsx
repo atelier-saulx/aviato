56c42ecc58f84619295677656315cecfad7604af
Jim de Beer <beerdejim@gmail.com>
2022-03-31 16:47:40 +0200

    undefined

--- packages/ui/src/components/Input/Select/Dropdown.tsx
+++ packages/ui/src/components/Input/Select/Dropdown.tsx
@@ -1,69 +1,71 @@
-import React, { FunctionComponent } from 'react'
-import { noop } from '@aviato/utils'
+// import React, { FunctionComponent } from 'react'
+// import { noop } from '@aviato/utils'
 
-import { onChange } from '~/types'
-import { SelectItem } from './types'
-import { ContextMenu, ContextItem } from '~/components/Overlay'
-import { Popover } from '~/components'
-import { getZIndex } from '~/theme'
+// import { onChange } from '~/types'
+// import { SelectItem } from './types'
+// import { ContextMenu, ContextItem } from '~/components/Overlay'
+// import { Popover } from '~/components'
+// import { getZIndex } from '~/theme'
 
-export interface OnDropdownChange extends onChange {
-  value: string | SelectItem
-}
+// export interface OnDropdownChange extends onChange {
+//   value: string | SelectItem
+// }
 
-export interface DropdownMenuProps {
-  items: SelectItem[]
-  referenceElement?: HTMLElement
-  mounted: boolean
-  zIndex?: number
-  uuid: string
-  onChange?: (value: string, payload: OnDropdownChange) => void
-}
+// export interface DropdownMenuProps {
+//   items: SelectItem[]
+//   referenceElement?: HTMLElement
+//   mounted: boolean
+//   zIndex?: number
+//   uuid: string
+//   onChange?: (value: string, payload: OnDropdownChange) => void
+// }
 
-export const Dropdown: FunctionComponent<DropdownMenuProps> = (properties) => {
-  const {
-    items,
-    onChange = noop,
-    referenceElement,
-    mounted,
-    uuid,
-    zIndex = getZIndex('Popover'),
-  } = properties
+// export const Dropdown: FunctionComponent<DropdownMenuProps> = (properties) => {
+//   const {
+//     items,
+//     onChange = noop,
+//     referenceElement,
+//     mounted,
+//     uuid,
+//     zIndex = getZIndex('Popover'),
+//   } = properties
 
-  const handleSelect = (event, { value, index, disabled }) => {
-    if (disabled) {
-      return
-    }
+//   const handleSelect = (event, { value, index, disabled }) => {
+//     if (disabled) {
+//       return
+//     }
 
-    onChange(value, { event, value, index })
-  }
+//     onChange(value, { event, value, index })
+//   }
 
-  const MenuItems = items.map(({ value, label, disabled }, index) => (
-    <ContextItem
-      key={`DropdownItem-${value}-${index}`}
-      onMouseDown={(event) => {
-        handleSelect(event, { value, index, disabled })
-      }}
-    >
-      {label}
-    </ContextItem>
-  ))
+//   const MenuItems = items.map(({ value, label, disabled }, index) => (
+//     <ContextItem
+//       key={`DropdownItem-${value}-${index}`}
+//       onClick={(event) => {
+//         handleSelect(event, { value, index, disabled })
+//       }}
+//     >
+//       {label}
+//     </ContextItem>
+//   ))
 
-  return (
-    <Popover
-      referenceElement={referenceElement}
-      mounted={mounted}
-      position="bottom"
-      placement="start"
-      zIndex={zIndex}
-      modifiers={[
-        {
-          name: 'preventOverflow',
-          enabled: false,
-        },
-      ]}
-    >
-      <ContextMenu id={`${uuid}-items`}>{MenuItems}</ContextMenu>
-    </Popover>
-  )
-}
+//   return (
+//     <Popover
+//       referenceElement={referenceElement}
+//       mounted={mounted}
+//       position="bottom"
+//       placement="start"
+//       zIndex={zIndex}
+//       modifiers={[
+//         {
+//           name: 'preventOverflow',
+//           enabled: false,
+//         },
+//       ]}
+//     >
+//       {/* <ContextMenu>{MenuItems}</ContextMenu> */}
+//     </Popover>
+//   )
+// }
+
+export const Dropdown = () => 'x'
