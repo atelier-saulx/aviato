631f52bf188982f8fe6d713d28a0999046c52519
youzi <youri@saulx.com>
2022-04-18 20:17:23 +0200

    add packages/ui/src/hooks/useObjectState/useObjectState.ts

--- packages/ui/src/hooks/useObjectState/index.ts
+++ packages/ui/src/hooks/useObjectState/index.ts
@@ -1,1 +1,2 @@
 export * from './useObjectState'
+export * from './useGlobalObjectState'
--- packages/ui/src/hooks/useObjectState/useObjectState.ts
+++ packages/ui/src/hooks/useObjectState/useObjectState.ts
@@ -1,31 +1,30 @@
-import { useRef } from 'react'
+import { useReducer, useRef } from 'react'
 import { deepMerge } from '@saulx/utils'
 import { hash } from '@saulx/hash'
 import useGlobalState from '@based/use-global-state'
 
 type GenericObject = { [key: string]: any }
 
-export function useGlobalObjectState<T = GenericObject>(
-  key: string,
+const updateReducer = (
+  state: { state: GenericObject; id?: number } = { state: {} },
+  action: GenericObject | null | undefined
+): GenericObject => {
+  if (!action) {
+    return { state: {} }
+  } else {
+    const n = deepMerge(state.state, action)
+    const nId = hash(n)
+    if (nId !== state.id) {
+      state.id = nId
+      return { ...state }
+    }
+  }
+  return state
+}
+
+export function useObjectState<T = GenericObject>(
   initialState?: T
 ): [T, (val: T | null | undefined) => void] {
-  const [globalState, setGlobalState] = useGlobalState(key, initialState)
-  const idRef = useRef<any>()
-
-  return [
-    globalState,
-    (val) => {
-      if (val) {
-        const merged = deepMerge(globalState, val)
-        const nId = hash(merged)
-        if (idRef.current !== nId) {
-          idRef.current = nId
-          return setGlobalState(key, { ...merged })
-        }
-      } else {
-        idRef.current = null
-        return setGlobalState(key, {})
-      }
-    },
-  ]
+  const [s, update] = useReducer(updateReducer, { state: initialState || {} })
+  return [s.state, update]
 }
